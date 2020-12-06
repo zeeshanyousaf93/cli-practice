5529cc436850635a01d2ed659520146d3bda1f41
Jesse Mykolyn <jrmykolyn@gmail.com>
2020-12-06 15:18:39 -0500

    Add information re: steps to entrypoint file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 // This file will contain the primary logic for the currency conversion program.
+// To run the program use the `node` command followed by the name of this file.
+// ie. `node index.js`.
+
+// This file has been split up into several sections, each of which focuses on a
+// portion of the program. Completing each of these sections in order should result
+// in a functional, testable program. However, please free to approach the problem
+// differently. There are many paths and approaches that result in a perfectly
+// valid finished product.
+
+// --------------------------------------------------
+// Step 1: Capture user input
+// --------------------------------------------------
+
+
+
+// --------------------------------------------------
+// Step 2: Validate user input
+// --------------------------------------------------
+
+
+
+// --------------------------------------------------
+// Step 3: Define currency conversion rates
+// --------------------------------------------------
+
+
+
+// --------------------------------------------------
+// Step 4: Ensure that a conversion rate exists
+// --------------------------------------------------
+
+
+
+// --------------------------------------------------
+// Step 5: Perform conversion
+// --------------------------------------------------
+
+
+
+// --------------------------------------------------
+// Step 6: Display results
+// --------------------------------------------------
