561a5bdce9b85209e3c8feb9245ae5e23aa39d1f
Jesse Mykolyn <jrmykolyn@gmail.com>
2020-12-06 15:11:03 -0500

    Add example test

--- test/index.test.js
+++ test/index.test.js
@@ -1,2 +1,23 @@
-// This file will contain tests for the program. The purpoes of these tests is to
+// This file will contain tests for the program. The purpose of these tests is to
 // ensure that the core program logic works as expected.
+
+// This project uses a testing library called Jest. You can learn more about Jest
+// here: https://jestjs.io/.
+
+// Test files can contain multiple tests, so long as those tests are grouped logically.
+// Included below is a function and a test that makes assertions about the behaviour
+// of the function. In most cases the subject of a test will be defined in a separate
+// file. In this case we've defined the function and the corresponding test in the
+// same file for illustrative and learning purposes.
+
+function myCoolFunction() {
+  return 'Wow, what a cool function';
+}
+
+describe('myCoolFunction()', () => {
+  test('should return the message: "Wow, what a cool function"', () => {
+    const result = myCoolFunction();
+
+    expect(result).toBe('Wow, what a cool function');
+  });
+});
